ece3c6314c9da0498fab86460026c793115c945f
claws-of-kodiak <mrbest2002@gmail.com>
2026-09-16 11:03:19 -0600

    Set Up ExpressJS server index file

--- the-blockchain-book/apps/server/src/server.ts
+++ the-blockchain-book/apps/server/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { cors } from "cors";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`You made a GET request to port: ${port}`);
+});
 
 app.listen(port, () => console.log("Server is live at port ", port));
