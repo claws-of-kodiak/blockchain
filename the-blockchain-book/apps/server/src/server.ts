1da593a46a8f1d72d9c23380b5cfaea591b64ff1
claws-of-kodiak <mrbest2002@gmail.com>
2026-09-16 11:00:57 -0600

    Configured: validations, frontend, and server

---
+++ the-blockchain-book/apps/server/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { cors } from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => console.log("Server is live at port ", port));
