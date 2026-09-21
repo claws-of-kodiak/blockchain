d62b032f620fe2c17b103194456afd86ad8b83f0
claws-of-kodiak <mrbest2002@gmail.com>
2026-09-21 10:51:21 -0600

    Create route to get user progress

--- the-blockchain-book/apps/server/src/routes/course.ts
+++ the-blockchain-book/apps/server/src/routes/course.ts
@@ -5,8 +5,16 @@ import pool from "../db";
 const courseRouter = express.Router();
 const courseRepo = new CourseRepository(pool);
 
+courseRouter.get("/course/userProgress/:id", async (req, res) => {
+  const userId: string = req.params.id;
+  const currentStep = await courseRepo.getProgressById(userId);
+  if (currentStep === null)
+    return res.status(404).json({ message: "No user progress found." });
+  return res.status(200).json({ currentStep, message: "User progress found." });
+});
+
 courseRouter.post("/begin", async (req, res) => {
-  const userId = req.body.userId;
+  const userId: string = req.body.userId;
   console.log("req.body.userID", userId);
   // ◻ Check if userId already exists in db
   const time = await courseRepo.beginCourse(userId);
